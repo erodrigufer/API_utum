51cebb0c6532626d4ca2b361afa91b0cd80ea9c9
erodrigufer <erodrigufer@gmail.com>
2022-03-28 19:54:10 +0200

    new routing with httprouter package

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -9,7 +9,7 @@ import (
 	"time"
 )
 
-const version = "1.0.0"
+const version = "0.1.0"
 
 type config struct {
 	port int
@@ -24,27 +24,21 @@ type application struct {
 }
 
 func main() {
-	// Declare an instance of the config struct.
 	var cfg config
-	// Read the value of the port and env command-line flags into the config struct. We // default to using the port number 4000 and the environment "development" if no
-	// corresponding flags are provided.
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
 	flag.Parse()
-	// Initialize a new logger which writes messages to the standard out stream, // prefixed with the current date and time.
+
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
-	// Declare an instance of the application struct, containing the config struct and // the logger.
+
 	app := &application{
 		config: cfg,
 		logger: logger,
 	}
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
-
 	srv := &http.Server{
 		Addr:         fmt.Sprintf(":%d", cfg.port),
-		Handler:      mux,
+		Handler:      app.routes(),
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/erodrigufer/API_utum
 
 go 1.17
+
+require github.com/julienschmidt/httprouter v1.3.0
